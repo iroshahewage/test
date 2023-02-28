3b4b341ba2167c20ee7b4bc8f855581d4a7c914b
iroshahewage <hewageirosha@gmail.com>
2023-02-28 20:49:52 +0530

    jwt add

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 dotenv.config();
 
 const app = express();
 
+var token = jwt.sign({ foo: "bar" }, process.env.PRIVATE_ACCESS_KEY, {
+  algorithm: "RS256",
+});
+
 app.get("/", (req, res) => {
-  res.send(process.env.PRIVATE_ACCESS_KEY);
+  res.send(token);
 });
 
 app.listen(8000, () => {
